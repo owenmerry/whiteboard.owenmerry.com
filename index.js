27f430bf85f391f72d2f0c2f4a0c130edb2b1838
Owen Merry <owenmerry@Owens-MacBook-Air.local>
2020-05-04 17:13:22 +0100

    first commit

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3004;
 
-var users = 0;
+app.use(express.static(__dirname + '/public'));
 
-//routes
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/pages/index.html');
-  });
+function onConnection(socket){
+  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+}
 
-//socket
-io.on('connection', (socket) => {
-    users++;
-    socket.broadcast.emit('chat message', 'shh.. someone has entered the room ( '+ users +' users online now)');
-    console.log('a user connected (users'+ users +')');
-    io.emit('chat message', 'New user has joined the chat ( '+ users +' users online now)');
-    socket.on('disconnect', () => {
-        users--;
-        console.log('user disconnected (users'+ users +')');
-        io.emit('chat message', 'A user disconnected from the chat ( '+ users +' users online now)');
-    });
+io.on('connection', onConnection);
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-      });
-  });
-
-//server
-http.listen(3002, () => {
-  console.log('listening on *:3002');
-});
+http.listen(port, () => console.log('listening on port ' + port));
