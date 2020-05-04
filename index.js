71563c8a37bcfde95261c520cb9bf0ab5d281e3e
Owen Merry <owenmerry@Owens-MacBook-Air.local>
2020-05-04 22:20:50 +0100

    added var

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3004;
-let onlineUsers = 0;
+var onlineUsers = 0;
 
 app.use(express.static(__dirname + '/public'));
 
