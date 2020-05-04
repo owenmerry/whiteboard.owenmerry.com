63b42c3feb71647667a0275e3204196620e753dc
Owen Merry <owenmerry@Owens-MacBook-Air.local>
2020-05-04 15:21:55 +0100

    changed port number

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ io.on('connection', (socket) => {
   });
 
 //server
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(3002, () => {
+  console.log('listening on *:3002');
 });
